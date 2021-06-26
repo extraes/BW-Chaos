1f3f027ed6f2e7d09d684b77e548d876bd9cbcd1
extraes <extraes@DESKTOP-ORBOGCQ>
2021-06-26 00:07:50 -0700

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle(BW_Chaos.BuildInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(BW_Chaos.BuildInfo.Company)]
+[assembly: AssemblyProduct(BW_Chaos.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + BW_Chaos.BuildInfo.Author)]
+[assembly: AssemblyTrademark(BW_Chaos.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+//[assembly: Guid("")]
+[assembly: AssemblyVersion(BW_Chaos.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(BW_Chaos.BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonModInfo(typeof(BW_Chaos.BW_Chaos), BW_Chaos.BuildInfo.Name, BW_Chaos.BuildInfo.Version, BW_Chaos.BuildInfo.Author, BW_Chaos.BuildInfo.DownloadLink)]
+
+
+// Create and Setup a MelonModGame to mark a Mod as Universal or Compatible with specific Games.
+// If no MelonModGameAttribute is found or any of the Values for any MelonModGame on the Mod is null or empty it will be assumed the Mod is Universal.
+// Values for MelonModGame can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonModGame(null, null)]
