975cbdb8ed470d52879e4914d0a057941a32ee52
extraes <52384576+extraes@users.noreply.github.com>
2021-07-28 19:29:48 -0700

    add pull all grips effect

---
+++ BW Chaos/Effects/World/PullAllGrips.cs
@@ -0,0 +1,28 @@
+﻿using MelonLoader;
+using StressLevelZero.Interaction;
+using System.Collections;
+using UnityEngine;
+
+namespace BWChaos.Effects
+{
+    internal class PullAllGrips : EffectBase
+    {
+        public PullAllGrips() : base("Force pull all grips", 30) { }
+
+        private object CToken = null;
+        public override void OnEffectStart() => CToken = MelonCoroutines.Start(ActivateGrips());
+        public override void OnEffectEnd() => MelonCoroutines.Stop(CToken);
+
+        public IEnumerator ActivateGrips()
+        {
+            ForcePullGrip[] grips = GameObject.FindObjectsOfType<ForcePullGrip>();
+            while (Active)
+            {
+                foreach (ForcePullGrip grip in grips)
+                    grip.Pull(Utilities.GetRandomPlayerHand());
+                yield return new WaitForEndOfFrame();
+            }
+        }
+
+    }
+}
