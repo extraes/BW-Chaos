d34eff54b8f88a110a7ccaf058ccc9f166831cf6
extraes <52384576+extraes@users.noreply.github.com>
2021-08-02 17:56:13 -0700

    proper attribution

--- BW Chaos/MelonLoaderMod.cs
+++ BW Chaos/MelonLoaderMod.cs
@@ -17,7 +17,7 @@ namespace BWChaos
     public static class BuildInfo
     {
         public const string Name = "BWChaos";
-        public const string Author = "extraes";
+        public const string Author = "extraes, trev";
         public const string Company = null;
         public const string Version = "0.1.2";
         public const string DownloadLink = null;      
