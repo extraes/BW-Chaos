7ea16d917dc3dddb8996ddefbd2d108d58403e8b
extraes <52384576+extraes@users.noreply.github.com>
2021-08-16 11:18:51 -0700

    addreadd changesteamname and randomslowmo
    also apparently randomslowmo's ienumerator was changeIime. i. not t. i dont think i wrote it at 3am either, idk man

---
+++ BW Chaos/Effects/Misc/ChangeSteamName.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using MelonLoader;
+
+namespace BWChaos.Effects
+{
+    internal class ChangeSteamName: EffectBase
+    {
+        // Shortened to 2m to avoid the effect running twice
+        public ChangeSteamName() : base("Change Steam name for 2m", 120, EffectTypes.USE_STEAM | EffectTypes.AFFECT_STEAM_PROFILE) { }
+
+        private string[] names = new string[] {
+            "Chester Chucklenuts",
+            "Your little PogChamp",
+            "✨ 💓💞❤️ Deku X Bakugo ❤️💞💓 ✨",
+            "Sydney|14|Bi|They/Them|BLM|ACAB",
+            "lars|gay|transmasc|allosexual/poly|libra|ravenclaw",
+            "xXx_DD0S_H4XX_xXx",
+            "Oragani",
+            "4K African",
+            "Brayden|32|ladies man|4'3\"|short kings stay winning",
+            "Brylan the wolf owo",
+            "Brylan Bristopher|CEO|LLC Owner|$DOGE HODLer🚀|Multimillionaire|Bossman, ❌suit ❌tie",
+            "xXAn0nym0usXx",
+            "shoutouts to simpleflips",
+            "Based, Redpilled | Dobe Johnson",
+            "Hack Dudes 69",
+            "Lawrence Albert Connor",
+            "Big. Black. River balls.", // i dont have the pass :woeis:
+        };
+
+        private string steamName = "helo :)";
+        public override void OnEffectStart()
+        {
+            steamName = Steamworks.SteamFriends.GetPersonaName();
+            Steamworks.SteamFriends.SetPersonaName($"{names[UnityEngine.Random.RandomRange(0, names.Length)]}-BWC");
+        }
+        
+        public override void OnEffectEnd()
+        {
+            Steamworks.SteamFriends.SetPersonaName(steamName);
+        }
+    }
+}
---
+++ BW Chaos/Effects/Misc/RandomTimeScale.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using MelonLoader;
+using System.Collections;
+using Random = UnityEngine.Random;
+
+namespace BWChaos.Effects
+{
+    internal class RandomTimeScale : EffectBase
+    {
+        public RandomTimeScale() : base("Random slowmo") { }
+
+        public override void OnEffectStart()
+        {
+            MelonCoroutines.Start(ChangeTime());
+        }
+        
+        public override void OnEffectEnd()
+        {
+        }
+
+        private IEnumerator ChangeTime()
+        {
+            yield return new WaitForSecondsRealtime(Random.RandomRange(6, 10));
+            if (!Active) yield break;
+
+            var times = new float[] { 0.125f, 0.25f, 0.5f };
+
+            Time.timeScale = times[Random.RandomRange(0, times.Length)];
+            yield return new WaitForSecondsRealtime(3);
+            Time.timeScale = 1;
+
+            _ = MelonCoroutines.Start(ChangeTime());
+        }
+    }
+}
