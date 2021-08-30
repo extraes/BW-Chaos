e46fc006f4c60500d12ca0810466be065beb7658
extraes <52384576+extraes@users.noreply.github.com>
2021-08-30 08:10:14 -0700

    add velocity inverting effects

---
+++ BW Chaos/Effects/Player/InvertPlayerVelocity.cs
@@ -0,0 +1,11 @@
+﻿namespace BWChaos.Effects
+{
+    internal class InvertPlayerVelocity : EffectBase
+    {
+        public InvertPlayerVelocity() : base("Invert player velocity") { }
+
+        public override void OnEffectStart() => 
+            GlobalVariables.Player_PhysBody.AddVelocityChange(-2 * GlobalVariables.Player_PhysBody.rbHead.velocity);
+        
+    }
+}
---
+++ BW Chaos/Effects/World/InvertVelocity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using MelonLoader;
+using System.Collections;
+
+namespace BWChaos.Effects
+{
+    internal class InvertVelocity : EffectBase
+    {
+        public InvertVelocity() : base("Invert velocity of everything") { }
+
+        public override void OnEffectStart()
+        {
+            MelonCoroutines.Start(CoRun());
+        }
+
+        private IEnumerator CoRun ()
+        {
+            foreach (var rb in GameObject.FindObjectsOfType<Rigidbody>())
+            {
+                if (rb.IsSleeping()) continue; // I don't fuck with the lames
+                // When subtractive 2x a number, you invert the number (e.g. 5 - (5*2) = -5)
+                rb.AddForce(-2 * rb.velocity, ForceMode.VelocityChange);
+                yield return new WaitForFixedUpdate();
+            }
+        }
+    }
+}
