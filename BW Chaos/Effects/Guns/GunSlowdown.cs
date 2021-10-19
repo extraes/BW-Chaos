b241e992efa611d3b6ee8b37cfeb4aaf6bd7880a
extraes <52384576+extraes@users.noreply.github.com>
2021-10-18 20:57:26 -0700

    Delete RPM effects, replace with slowdown/speedup ones

---
+++ BW Chaos/Effects/Guns/GunSlowdown.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using MelonLoader;
+using ModThatIsNotMod;
+
+namespace BWChaos.Effects
+{
+    class GunSlowdown : EffectBase
+    {
+        public GunSlowdown() : base("Progressively slower guns", 60) { }
+
+        public override void OnEffectStart() => Hooking.OnPostFireGun += OnGunFired;
+        public override void OnEffectEnd() => Hooking.OnPostFireGun -= OnGunFired;
+
+        public void OnGunFired (StressLevelZero.Props.Weapons.Gun gun)
+        {
+            if (gun == Player.GetGunInHand(Player.rightHand) || gun == Player.GetGunInHand(Player.leftHand))
+            {
+                gun.SetRpm(gun.roundsPerMinute * 0.99f);
+            }
+        }
+    }
+}
