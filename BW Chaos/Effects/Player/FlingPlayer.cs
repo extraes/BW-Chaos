6696bcf1d8b4268b135102a9edd5c1b0cec146f7
extraes <52384576+extraes@users.noreply.github.com>
2021-11-27 14:07:28 -0800

    readjust flingplayer for the 90th time

--- BW Chaos/Effects/Player/FlingPlayer.cs
+++ BW Chaos/Effects/Player/FlingPlayer.cs
@@ -6,12 +6,13 @@ namespace BWChaos.Effects
     internal class FlingPlayer : EffectBase
     {
         public FlingPlayer() : base("Fling Player") { }
+        readonly int[] arr = new int[] { -1, 1 };
 
         public override void OnEffectStart()
         {
-            int[] arr = new int[] { -1, 1 };
+            
             GlobalVariables.Player_PhysBody.AddVelocityChange(
-                new Vector3(9.8f * 2 * arr[Random.Range(0, 2)], 9.8f * 4, 9.8f * 2 * arr[Random.Range(0, 2)]));
+                new Vector3(9.8f * 1 * arr.Random(), 9.8f * 1.5f, 9.8f * 1 * arr.Random()));
         }
     }
 }
