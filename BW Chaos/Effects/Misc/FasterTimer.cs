50e576abafec130218596da7f1d7810b483c6570
extraes <52384576+extraes@users.noreply.github.com>
2021-11-27 14:00:55 -0800

    add flattencameras from a lakatrazz suggestion and add first meta effect cause why not

---
+++ BW Chaos/Effects/Misc/FasterTimer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using MelonLoader;
+using ModThatIsNotMod;
+using System.Collections;
+using System.Reflection;
+
+namespace BWChaos.Effects
+{
+    internal class FastTimer : EffectBase
+    {
+        public FastTimer() : base("[Meta] Fast Effect Timer", 90) { }
+        static readonly FieldInfo secInfo = typeof(EffectHandler).GetField("secondsEachEffect", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        public override void OnEffectStart() => secInfo.SetValue(EffectHandler.Instance, 15);
+        
+        public override void OnEffectEnd() => secInfo.SetValue(EffectHandler.Instance, 30);
+    }
+}
---
+++ BW Chaos/Effects/Player/FlattenCameras.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using MelonLoader;
+using ModThatIsNotMod;
+using System.Collections;
+
+namespace BWChaos.Effects
+{
+    internal class FlattenDepthPerception : EffectBase
+    {
+        public FlattenDepthPerception() : base("Flatten Depth Perception", 30) { }
+
+        public override void OnEffectStart()
+        {
+            foreach (var cam in GlobalVariables.Cameras)
+            {
+                cam.transform.localScale = new Vector3(0, 1, 1);
+                cam.fieldOfView *= 0.75f;
+            }
+        }
+        public override void OnEffectEnd()
+        {
+            foreach (var cam in GlobalVariables.Cameras)
+            {
+                cam.transform.localScale = Vector3.one;
+                cam.fieldOfView /= 0.75f;
+            }
+        }
+    }
+}
