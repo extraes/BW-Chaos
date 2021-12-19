162831e42c12bc2965c801a0c473de1e34f145d6
extraes <52384576+extraes@users.noreply.github.com>
2021-12-19 13:17:17 -0800

    overdo syncing for gullible

--- BW Chaos/Effects/Misc/GullibleOnTheCeiling.cs
+++ BW Chaos/Effects/Misc/GullibleOnTheCeiling.cs
@@ -3,19 +3,26 @@ using UnityEngine;
 using MelonLoader;
 using ModThatIsNotMod;
 using ModThatIsNotMod.RandomShit;
+using System.Collections.Generic;
 
 namespace BWChaos.Effects
 {
     internal class ItSaysGullibleOnTheCeiling : EffectBase
     {
         public ItSaysGullibleOnTheCeiling() : base("It says gullible on the ceiling", 60) { }
-
-        GameObject sign = null;
+        // why a ushort? idk.
+        Dictionary<ushort, GameObject> signs = new Dictionary<ushort, GameObject>();
+        ushort myID;
         Transform pHead;
         public override void OnEffectStart()
         {
             pHead = GlobalVariables.Player_PhysBody.rbHead.transform;
-            sign = AdManager.CreateNewAd("gullible");
+            var sign = AdManager.CreateNewAd("gullible");
+            ushort signID = (ushort)UnityEngine.Random.Range(0,ushort.MaxValue);
+            signs.Add(signID, sign);
+            myID = signID;
+            SendNetworkData($"{signID};{sign.transform.position.Serialize(4).Join()}");
+
             GameObject.Destroy(sign.GetComponent<StressLevelZero.Props.ObjectDestructable>());
             GameObject.Destroy(sign.GetComponent<StressLevelZero.SFX.ImpactSFX>());
             GameObject.Destroy(sign.GetComponent<StressLevelZero.Interaction.InteractableHost>());
@@ -23,10 +30,36 @@ namespace BWChaos.Effects
         }
         public override void OnEffectUpdate()
         {
-            if (sign == null) return; // In case something goes wrong
-            sign.transform.position = pHead.position + Vector3.up * 2;
-            sign.transform.rotation = Quaternion.LookRotation(Vector3.up);
+            if (signs.TryGetValue(myID, out var sign) && sign != null)
+            {
+                if (sign == null) return; // In case something goes wrong
+                sign.transform.position = pHead.position + Vector3.up * 2;
+                sign.transform.rotation = Quaternion.LookRotation(Vector3.up);
+                if (Time.frameCount % 4 == 0) SendNetworkData($"{myID};{sign.transform.position.Serialize(4).Join()}");
+            }
         }
-        public override void OnEffectEnd() => GameObject.Destroy(sign);
+
+        // fucking overbuilt shit to support multiple gullible signs
+        public override void HandleNetworkMessage(string data)
+        {
+            string[] datas = data.Split(';');
+
+            ushort id = ushort.Parse(datas[0]);
+            Vector3 pos = Utilities.DeserializeV3(datas[1]);
+            if (!signs.TryGetValue(id, out var sign))
+            {
+                var newSign = AdManager.CreateNewAd("gullible");
+                GameObject.Destroy(newSign.GetComponent<StressLevelZero.Props.ObjectDestructable>());
+                GameObject.Destroy(newSign.GetComponent<StressLevelZero.SFX.ImpactSFX>());
+                GameObject.Destroy(newSign.GetComponent<StressLevelZero.Interaction.InteractableHost>());
+                sign.GetComponent<Rigidbody>().detectCollisions = false;
+
+                signs.Add(id, newSign);
+                newSign.transform.position = pos;
+            }
+            else sign.transform.position = pos;
+        }
+
+        public override void OnEffectEnd() => signs.Values.ForEach(s => s.Destroy());
     }
 }
