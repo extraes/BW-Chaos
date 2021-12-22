d6d8f8d0f32d8e53f0824f3470538256fffce9c0
extraes <52384576+extraes@users.noreply.github.com>
2021-12-22 00:29:25 -0800

    Thought I'd make you my guest, and light up the runway with something from my personal collection.

---
+++ BW Chaos/Effects/Player/GhostWorld.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using MelonLoader;
+using ModThatIsNotMod;
+using System.Collections;
+using Random = UnityEngine.Random;
+
+namespace BWChaos.Effects
+{
+    internal class GhostWorld : EffectBase
+    {
+        public GhostWorld() : base("Ghost World", 60) { }
+
+        public override void OnEffectStart() => GameObject.FindObjectsOfType<ValveCamera>().ForEach(c => c.m_hideAllValveMaterials = true);
+        
+        public override void OnEffectEnd() => GameObject.FindObjectsOfType<ValveCamera>().ForEach(c => c.m_hideAllValveMaterials = false);
+    }
+}
