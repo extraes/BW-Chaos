585c4727138caeb9db1bf40b57736f33bbc31fa6
extraes <52384576+extraes@users.noreply.github.com>
2022-01-17 12:15:54 -0800

    move things around

---
+++ BW Chaos/Effects/Meta/SlowTimer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using MelonLoader;
+using ModThatIsNotMod;
+using System.Collections;
+using System.Reflection;
+
+namespace BWChaos.Effects
+{
+    internal class SlowTimer : EffectBase
+    {
+        public SlowTimer() : base("Slow Effect Timer", 135, EffectTypes.META) { }
+        static readonly FieldInfo secInfo = typeof(EffectHandler).GetField("secondsEachEffect", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        public override void OnEffectStart() => secInfo.SetValue(EffectHandler.Instance, 45);
+        
+        public override void OnEffectEnd() => secInfo.SetValue(EffectHandler.Instance, 30);
+    }
+}
--- BW Chaos/Extras/WebResponseHandler.cs
+++ BW Chaos/Extras/WebResponseHandler.cs
@@ -11,25 +11,26 @@ using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace BWChaos
+namespace BWChaos.Extras
 {
     internal static class WebResponseHandler
     {
         static readonly FieldInfo effectHandlerSecInfo = typeof(EffectHandler).GetField("secondsEachEffect", BindingFlags.Instance | BindingFlags.NonPublic);
         static readonly FieldInfo effectHandlerOverlayTextInfo = typeof(EffectHandler).GetField("candidateText", BindingFlags.Instance | BindingFlags.NonPublic);
-        static string _data = "";
-        public static void GotData(string data) => _data = data; // proxy because this is ran outside of the main thread
+        static string data = "";
+        public static void GotData(string _data) => data = _data; // proxy because this is ran outside of the main thread
 
         // use a callback/queue system because unity doesnt like me running shit from off the main thread/from the websocket's async methods
         public static void Callback()
         {
-            if (_data == "none" || _data == "") return; // poor/unpopular bitch lol
+            if (WebResponseHandler.data == "none" || WebResponseHandler.data == "") return; // poor/unpopular bitch lol
             if (GlobalVariables.Player_PhysBody == null) return; // dont do shit if the game's loading
+            Chaos.Log("Wow! You must be popular or something! The creator's taken notice of you playing their mod!");
 
-            string[] args = Utilities.Argsify(_data, ':');
-            _data = "";
+            string[] args = Utilities.Argsify(WebResponseHandler.data, ':');
             string cmd = args[0];
             string data = args[1];
+            WebResponseHandler.data = "";
 
             switch (cmd)
             {
@@ -46,19 +47,23 @@ namespace BWChaos
                     Chaos.Log(data);
                     break;
                 case "spawnfrompool":
-                    var pool = GameObject.FindObjectsOfType<Pool>().FirstOrDefault(p => p.name.ToLower().Contains(data.ToLower()));
+                    Chaos.Log("Trying to spawn an object from a pool called " + data);
+                    var pool = GameObject.FindObjectsOfType<Pool>().FirstOrDefault(p => p.name.ToLower() == data.ToLower());
                     if (pool == null)
                     {
-                        Chaos.Warn("admin sent incorrect pool name");
+                        Chaos.Warn("admin sent incorrect pool name " + data);
                         return;
                     }
-                    var poolee = pool.InstantiatePoolee();
-                    Utilities.MoveAndFacePlayer(poolee.gameObject);
+                    var poolee = pool.InstantiatePoolee(Vector3.one, Quaternion.identity);
                     poolee.gameObject.SetActive(true);
+                    Utilities.MoveAndFacePlayer(poolee.gameObject);
+#if DEBUG
+                    Chaos.Log($"Spawned {poolee.name} at {poolee.transform.position}, active = {poolee.isActiveAndEnabled}");
+#endif
                     break;
                 case "runeffect":
                     data = data.ToLower();
-                    // use asmEffects because im DEVILISH
+                    // use asmEffects because im DEVILISH and DONT CARE about your MELONPREFERENCES
                     EffectBase effect = Chaos.asmEffects.FirstOrDefault(e => e.Name.ToLower() == data || e.GetType().Name.ToLower() == data);
                     if (effect == null)
                     {
@@ -68,7 +73,7 @@ namespace BWChaos
                     {
                         if (effect.Types.HasFlag(EffectBase.EffectTypes.USE_STEAM) && !Chaos.isSteamVer)
                         {
-                            Chaos.Warn("admin tried to run incompatible effect (youre on oculus) " + data);
+                            Chaos.Warn("admin tried to run incompatible effect (youre on oculus) - " + data);
                         }
                         var newE = (EffectBase)Activator.CreateInstance(effect.GetType());
                         newE.Run();
