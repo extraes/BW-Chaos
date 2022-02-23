41ff34a14e07ac02f3ff5de45d74a5237e987d5b
extraes <52384576+extraes@users.noreply.github.com>
2022-02-23 08:01:26 -0800

    add 2 new effects

---
+++ BW Chaos/Effects/Player/Cage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+using MelonLoader;
+using ModThatIsNotMod;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using Random = UnityEngine.Random;
+
+namespace BWChaos.Effects
+{
+    internal class Cage : EffectBase
+    {
+        public Cage() : base("Cage", 15) { }
+        static GameObject cagePrefab;
+        GameObject cage;
+        static Cage()
+        {
+            cagePrefab = GlobalVariables.EffectResources.LoadAsset<GameObject>("Assets/Cage/cage.prefab");
+            cagePrefab.hideFlags = HideFlags.DontUnloadUnusedAsset;
+            GameObject.DontDestroyOnLoad(cagePrefab);
+        }
+
+        public override void HandleNetworkMessage(byte[] data)
+        {
+            cage.transform.position = Utilities.DebyteV3(data);
+        }
+
+        public override void OnEffectStart()
+        {
+            cage = GameObject.Instantiate(cagePrefab);
+            if (isNetworked) return;
+            cage.transform.position = GlobalVariables.Player_PhysBody.rbFeet.position;
+            SendNetworkData(cage.transform.position.ToBytes());
+        }
+
+        public override void OnEffectEnd()
+        {
+            cage.Destroy();
+        }
+    }
+}
---
+++ BW Chaos/Effects/World/Fold.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using MelonLoader;
+using ModThatIsNotMod;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using Random = UnityEngine.Random;
+using StressLevelZero.AI;
+using PuppetMasta;
+
+namespace BWChaos.Effects
+{
+    internal class Fold : EffectBase
+    {
+        public Fold() : base("<b>Fold.</b>") { }
+
+        public override void OnEffectStart()
+        {
+            try
+            {
+                foreach (var pm in GameObject.FindObjectsOfType<PuppetMaster>())
+                {
+                    pm.Kill();
+                }
+            }
+            catch { }
+        }
+        
+    }
+}
