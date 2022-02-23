b9bcb5516b83d325b48333b7fa32b449a717ab62
extraes <52384576+extraes@users.noreply.github.com>
2022-02-23 00:26:54 -0800

    use melonpreferences_entry now

--- BW Chaos/Prefs.cs
+++ BW Chaos/Prefs.cs
@@ -7,17 +7,37 @@ namespace BWChaos
 {
     public static class Prefs
     {
-        internal static bool UseLaggyEffects { get; private set; } = false;
-        internal static bool UseGravityEffects { get; private set; } = false;
-        internal static bool UseSteamProfileEffects { get; private set; } = false;
-        internal static bool UseMetaEffects { get ; private set; } = true;
-        internal static bool ShowCandidatesOnScreen { get; private set; } = true;
-        internal static bool ShowWristUI { get; private set; } = true;
-        internal static bool SendCandidatesInChannel { get; private set; } = true;
-        internal static bool IgnoreRepeatVotes { get; private set; } = false;
-        internal static bool ProportionalVoting { get; private set; } = true;
-        internal static bool EnableRemoteVoting { get; private set; } = false;
-        internal static bool SyncEffects { get; private set; } = false;
+        internal const string CATEGORY_NAME = "BW_Chaos";
+        private static MelonPreferences_Category category;
+        internal static MelonPreferences_Entry<bool> randomOnNoVotes;
+        internal static MelonPreferences_Entry<bool> useLaggyEffects;
+        internal static MelonPreferences_Entry<bool> useGravityEffects;
+        internal static MelonPreferences_Entry<bool> useSteamProfileEffects;
+        internal static MelonPreferences_Entry<bool> useMetaEffects;
+        internal static MelonPreferences_Entry<bool> showCandidatesOnScreen;
+        internal static MelonPreferences_Entry<bool> scrollCandidates;
+        internal static MelonPreferences_Entry<bool> showWristUI;
+        internal static MelonPreferences_Entry<bool> sendCandidatesInChannel;
+        internal static MelonPreferences_Entry<bool> ignoreRepeatVotes;
+        internal static MelonPreferences_Entry<bool> proportionalVoting;
+        internal static MelonPreferences_Entry<bool> enableRemoteVoting;
+        internal static MelonPreferences_Entry<bool> syncEffects;
+        internal static MelonPreferences_Entry<bool> useBagRandomizer;
+
+        internal static bool RandomOnNoVotes => randomOnNoVotes.Value;
+        internal static bool UseLaggyEffects => useLaggyEffects.Value;
+        internal static bool UseGravityEffects => useGravityEffects.Value;
+        internal static bool UseSteamProfileEffects => useSteamProfileEffects.Value;
+        internal static bool UseMetaEffects => useMetaEffects.Value;
+        internal static bool ShowCandidatesOnScreen => showCandidatesOnScreen.Value;
+        internal static bool ScrollCandidates => scrollCandidates.Value;
+        internal static bool ShowWristUI => showWristUI.Value;
+        internal static bool SendCandidatesInChannel => sendCandidatesInChannel.Value;
+        internal static bool IgnoreRepeatVotes => ignoreRepeatVotes.Value;
+        internal static bool ProportionalVoting => proportionalVoting.Value;
+        internal static bool EnableRemoteVoting => enableRemoteVoting.Value;
+        internal static bool SyncEffects => syncEffects.Value;
+        internal static bool UseBagRandomizer => useBagRandomizer.Value;
         internal static List<string> ForceEnabledEffects { get; private set; } = new List<string>();
         internal static List<string> ForceDisabledEffects { get; private set; } = new List<string>();
 #if DEBUG
@@ -26,49 +46,41 @@ namespace BWChaos
 
         internal static void Init()
         {
-            MelonPreferences.CreateEntry("BW_Chaos", "token", "YOUR_TOKEN_HERE", "token");
-            MelonPreferences.CreateEntry("BW_Chaos", "channel", "CHANNEL_ID_HERE", "channel");
-            MelonPreferences.CreateEntry("BW_Chaos", "randomEffectOnNoVotes", EffectHandler.randomOnNoVotes, "randomEffectOnNoVotes");
-            MelonPreferences.CreateEntry("BW_Chaos", "useGravityEffects", UseGravityEffects, "useGravityEffects");
-            MelonPreferences.CreateEntry("BW_Chaos", "useSteamProfileEffects", UseSteamProfileEffects, "useSteamProfileEffects");
-            MelonPreferences.CreateEntry("BW_Chaos", "useLaggyEffects", UseLaggyEffects, "useLaggyEffects");
-            MelonPreferences.CreateEntry("BW_Chaos", "useMetaEffects", UseMetaEffects, "useMetaEffects");
+            category = MelonPreferences.CreateCategory(CATEGORY_NAME);
+            
+            MelonPreferences.CreateEntry(CATEGORY_NAME, "token", "YOUR_TOKEN_HERE", "token");
+            MelonPreferences.CreateEntry(CATEGORY_NAME, "channel", "CHANNEL_ID_HERE", "channel");
+            randomOnNoVotes = category.CreateEntry("randomEffectOnNoVotes", true);
+            useGravityEffects = category.CreateEntry("useGravityEffects", false);
+            useSteamProfileEffects = category.CreateEntry("useSteamProfileEffects", false);
+            useLaggyEffects = category.CreateEntry("useLaggyEffects", UseLaggyEffects);
+            useMetaEffects = category.CreateEntry("useMetaEffects", UseMetaEffects);
             // voteprefs
-            MelonPreferences.CreateEntry("BW_Chaos", "showCandidatesOnScreen", ShowCandidatesOnScreen, "showCandidatesOnScreen");
-            MelonPreferences.CreateEntry("BW_Chaos", "showWristUI", ShowWristUI, "showWristUI");
-            MelonPreferences.CreateEntry("BW_Chaos", "sendCandidatesInChannel", SendCandidatesInChannel, "sendCandidatesInChannel");
-            MelonPreferences.CreateEntry("BW_Chaos", "ignoreRepeatVotesFromSameUser", IgnoreRepeatVotes, "ignoreRepeatVotesFromSameUser");
-            MelonPreferences.CreateEntry("BW_Chaos", "proportionalVoting", ProportionalVoting, "proportionalVoting");
-            MelonPreferences.CreateEntry("BW_Chaos", "enableRemoteVoting", EnableRemoteVoting, "enableRemoteVoting");
+            showCandidatesOnScreen = category.CreateEntry("showCandidatesOnScreen", true);
+            scrollCandidates = category.CreateEntry("scrollCandidates", true);
+            showWristUI = category.CreateEntry("showWristUI", true);
+            sendCandidatesInChannel = category.CreateEntry("sendCandidatesInChannel", false);
+            ignoreRepeatVotes = category.CreateEntry("ignoreRepeatVotesFromSameUser", true);
+            proportionalVoting = category.CreateEntry("proportionalVoting", true);
+            enableRemoteVoting = category.CreateEntry("enableRemoteVoting", false);
             // end voteprefs :^)
-            MelonPreferences.CreateEntry("BW_Chaos", "syncEffectsViaEntanglement", SyncEffects, "syncEffectsViaEntanglement");
-            MelonPreferences.CreateEntry("BW_Chaos", "forceEnabledEffects", ForceEnabledEffects.ToArray(), "forceEnabledEffects");
-            MelonPreferences.CreateEntry("BW_Chaos", "forceDisabledEffects", ForceDisabledEffects.ToArray(), "forceDisabledEffects");
+            syncEffects = category.CreateEntry("syncEffectsViaEntanglement", false);
+            useBagRandomizer = category.CreateEntry("useBagRandomizer", false);
+            MelonPreferences.CreateEntry(CATEGORY_NAME, "forceEnabledEffects", ForceEnabledEffects.ToArray(), "forceEnabledEffects");
+            MelonPreferences.CreateEntry(CATEGORY_NAME, "forceDisabledEffects", ForceDisabledEffects.ToArray(), "forceDisabledEffects");
+            
 #if DEBUG
-            MelonPreferences.CreateEntry("BW_Chaos", "enableIMGUI", enableIMGUI, "enableIMGUI");
+            MelonPreferences.CreateEntry(CATEGORY_NAME, "enableIMGUI", enableIMGUI, "enableIMGUI");
 #endif
             MelonPreferences.Save();
         }
 
         public static void Get()
         {
-            EffectHandler.randomOnNoVotes = MelonPreferences.GetEntryValue<bool>("BW_Chaos", "randomEffectOnNoVotes");
-            UseGravityEffects = MelonPreferences.GetEntryValue<bool>("BW_Chaos", "useGravityEffects");
-            UseSteamProfileEffects = MelonPreferences.GetEntryValue<bool>("BW_Chaos", "useSteamProfileEffects");
-            UseLaggyEffects = MelonPreferences.GetEntryValue<bool>("BW_Chaos", "useLaggyEffects");
-            // Voting preferences
-            ShowCandidatesOnScreen = MelonPreferences.GetEntryValue<bool>("BW_Chaos", "showCandidatesOnScreen");
-            ShowWristUI = MelonPreferences.GetEntryValue<bool>("BW_Chaos", "showWristUI");
-            SendCandidatesInChannel = MelonPreferences.GetEntryValue<bool>("BW_Chaos", "sendCandidatesInChannel");
-            IgnoreRepeatVotes = MelonPreferences.GetEntryValue<bool>("BW_Chaos", "ignoreRepeatVotesFromSameUser");
-            ProportionalVoting = MelonPreferences.GetEntryValue<bool>("BW_Chaos", "proportionalVoting");
-            EnableRemoteVoting = MelonPreferences.GetEntryValue<bool>("BW_Chaos", "enableRemoteVoting");
-            // yeah what that comment said
-            SyncEffects = MelonPreferences.GetEntryValue<bool>("BW_Chaos", "syncEffectsViaEntanglement");
-            ForceEnabledEffects = MelonPreferences.GetEntryValue<string[]>("BW_Chaos", "forceEnabledEffects").ToList();
-            ForceDisabledEffects = MelonPreferences.GetEntryValue<string[]>("BW_Chaos", "forceDisabledEffects").ToList();
+            ForceEnabledEffects = MelonPreferences.GetEntryValue<string[]>(CATEGORY_NAME, "forceEnabledEffects").ToList();
+            ForceDisabledEffects = MelonPreferences.GetEntryValue<string[]>(CATEGORY_NAME, "forceDisabledEffects").ToList();
 #if DEBUG
-            enableIMGUI = MelonPreferences.GetEntryValue<bool>("BW_Chaos", "enableIMGUI");
+            enableIMGUI = MelonPreferences.GetEntryValue<bool>(CATEGORY_NAME, "enableIMGUI");
 #endif
 
             Chaos.eTypesToPrefs.Clear();
@@ -81,13 +93,14 @@ namespace BWChaos
                 (EffectTypes.HIDDEN, true),
                 (EffectTypes.DONT_SYNC, !SyncEffects),
                 (EffectTypes.META, UseMetaEffects),
+                (EffectTypes.DEFAULT_DISABLED, false),
             });
         }
 
         public static async void SendBotInitalValues()
         {
-            await GlobalVariables.WatsonClient.SendAsync("token:" + MelonPreferences.GetEntryValue<string>("BW_Chaos", "token"));
-            await GlobalVariables.WatsonClient.SendAsync("channel:" + MelonPreferences.GetEntryValue<string>("BW_Chaos", "channel").Trim());
+            await GlobalVariables.WatsonClient.SendAsync("token:" + MelonPreferences.GetEntryValue<string>(CATEGORY_NAME, "token"));
+            await GlobalVariables.WatsonClient.SendAsync("channel:" + MelonPreferences.GetEntryValue<string>(CATEGORY_NAME, "channel").Trim());
         }
     }
 }
