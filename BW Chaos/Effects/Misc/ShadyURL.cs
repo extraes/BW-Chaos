80504f7cdd1afcdbc27e5f06a6c7328c7b604d9f
extraes <52384576+extraes@users.noreply.github.com>
2024-07-23 13:53:26 -0700

    5z8.info is porn now. only use for display. open actual links instead

--- BW Chaos/Effects/Misc/ShadyURL.cs
+++ BW Chaos/Effects/Misc/ShadyURL.cs
@@ -6,38 +6,40 @@ namespace BWChaos.Effects;
 
 internal class ShadyURL : EffectBase
 {
+    const string SEPARATOR = "|||";
     public ShadyURL() : base("Shady URL", 15) { }
     [EffectPreference] static readonly bool showInSteamOverlayIfAvailable = true;
 
-    private readonly string[] shadyURLs = new string[] {
-        "http://www.5z8.info/begin-bank-account-xfer_o8u1tn_cockfights",            // https://www.youtube.com/embed/5IVsAcWqTc8?rel=0 GROUSE
-        "http://www.5z8.info/hitler_g4v2bu_peepshow",                               // http://endless.horse/ hooooooooooooooooooooooooooooooooooooooorse
-        "http://www.5z8.info/-php-deactivate_phishing_filter-48-_k0k8ee_smut",      // https://froggi.es/website/site/ I like Shirts (or whatever) god tier site tho
-        "http://www.5z8.info/56-DEPLOY-TROJAN-287.mw9----_g9v0ut_facebook-hack",    // https://longdogechallenge.com/ infinidoge
-        "http://www.5z8.info/barely-legal_r7p9ws_add-worm",                         // http://eelslap.com/ eelslap!
-        "http://www.5z8.info/hack-outlook_b9r3vo_boobs",                            // http://corndog.io/ a bunch of corndogs floating in space
-        "http://www.5z8.info/facebook-hack_k8o3fv_start-trojan"                     // https://bullsquid.com/ spinning bullsquid from hl1
+    private readonly (string, string)[] shadyURLs = new (string, string)[] {
+        ("http://www.5z8.info/begin-bank-account-xfer_o8u1tn_cockfights", "https://www.youtube.com/embed/5IVsAcWqTc8?rel=0"),   // https://www.youtube.com/embed/5IVsAcWqTc8?rel=0 GROUSE
+        ("http://www.5z8.info/-php-deactivate_phishing_filter-48-_k0k8ee_smut", "https://froggi.es/website/site/"),             // https://froggi.es/website/site/ I like Shirts (or whatever) god tier site tho
+        ("http://www.5z8.info/hitler_g4v2bu_peepshow", "http://endless.horse/"),                                                // http://endless.horse/ hooooooooooooooooooooooooooooooooooooooorse
+        ("http://www.5z8.info/56-DEPLOY-TROJAN-287.mw9----_g9v0ut_facebook-hack", "https://longdogechallenge.com/"),            // https://longdogechallenge.com/ infinidoge
+        ("http://www.5z8.info/barely-legal_r7p9ws_add-worm", "http://eelslap.com/"),                                            // http://eelslap.com/ eelslap!
+        ("http://www.5z8.info/hack-outlook_b9r3vo_boobs", "http://corndog.io/"),                                                // http://corndog.io/ a bunch of corndogs floating in space
+        ("http://www.5z8.info/facebook-hack_k8o3fv_start-trojan", "https://bullsquid.com/"),                                    // https://bullsquid.com/ spinning bullsquid from hl1
     };
 
     public override void HandleNetworkMessage(string data)
     {
-        MelonCoroutines.Start(CoShow(data));
+        string[] urls = data.Split(new string[] { SEPARATOR }, System.StringSplitOptions.None);
+        MelonCoroutines.Start(CoShow(urls[0], urls[1]));
     }
 
     [AutoCoroutine]
     public System.Collections.IEnumerator CoRun()
     {
         if (isNetworked) yield break;
-        string pickedURL = shadyURLs.Random();
-        SendNetworkData(pickedURL);
-        yield return CoShow(pickedURL);
+        (string showUrl, string actualUrl) = shadyURLs.Random();
+        SendNetworkData(showUrl + SEPARATOR + actualUrl);
+        yield return CoShow(showUrl, actualUrl);
     }
 
-    public System.Collections.IEnumerator CoShow(string url)
+    public System.Collections.IEnumerator CoShow(string showUrl, string openUrl)
     {
         SpawnAd("Hey wanna see one of my favorite websites");
         yield return new WaitForSeconds(4f);
-        SpawnAd(url);
+        SpawnAd(showUrl);
 
         if (Chaos.isSteamVer && showInSteamOverlayIfAvailable)
         {
@@ -45,7 +47,7 @@ internal class ShadyURL : EffectBase
             SpawnAd("Why don't I show you :^)");
             yield return new WaitForSeconds(5f);
 
-            Steamworks.SteamFriends.ActivateGameOverlayToWebPage(url, Steamworks.EActivateGameOverlayToWebPageMode.k_EActivateGameOverlayToWebPageMode_Default);
+            Steamworks.SteamFriends.ActivateGameOverlayToWebPage(openUrl, Steamworks.EActivateGameOverlayToWebPageMode.k_EActivateGameOverlayToWebPageMode_Default);
         }
     }
 }
